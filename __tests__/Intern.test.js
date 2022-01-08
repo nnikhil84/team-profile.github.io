9b3fa2c2025202bb799905d122bd507a9f73ea47
nnikhil <nikhil_narendran@hotmail.com>
2022-01-07 23:36:26 -0500

    minor correction

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -4,16 +4,16 @@ test("Can set school via constructor", () => {
   const testValue = "Stanford";
   const testvar = new Intern("Viveca", 1, "viveca@empire.com", testValue);
   expect(testvar.school).toBe(testValue);
-})
+});
 
 test('getRole() should return "Intern"', () => {
   const testValue = "Intern";
   const testvar = new Intern("Viveca", 1, "viveca@empire.com", "Stanford");
   expect(testvar.getRole()).toBe(testValue);
-})
+});
 
 test("Can get school via getSchool()", () => {
   const testValue = "Stanford";
   const testvar = new Intern("Viveca", 1, "viveca@empire.com", testValue);
   expect(testvar.getSchool()).toBe(testValue);
-})
+});
